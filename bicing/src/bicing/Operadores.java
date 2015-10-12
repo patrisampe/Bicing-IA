3003947717210b99582a2b58032cceb91aeb7854
Yoel Cabo <yoel.cabo@est.fib.upc.edu>
2015-10-12 12:38:48 +0200

    primeras funciones de operadores y sucesores

--- bicing/src/bicing/Operadores.java
+++ bicing/src/bicing/Operadores.java
@@ -1,66 +1,86 @@
 package bicing;
 
+import IA.Bicing.Estacion;
+
 public class Operadores {
 	
 	public static int num = 1;
-	public static Estado intercambiarE(Estado e, int f) {
+	public static Estado intercambiarE(Estado e, int f, Estacion a, Estacion b) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		e.intercambiarE(a, b);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
-	public static Estado modificarP1(Estado e, int f) {
+	public static Estado modificarP1(Estado e, int f, Estacion p) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		ret.canviarP1(f,p);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
-	public static Estado modificarP2(Estado e, int f) {
+	public static Estado modificarP2(Estado e, int f, Estacion p) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		ret.canviarP2(f,p);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado intercambiarP1P2(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.intercambiarP1P2(f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado incNeNp1(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.incrementarNNENP1(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado incNeNp2(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.incrementarNNENP2(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado decNeNp1(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.decrementarNNENP1(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado decNeNp2(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.decrementarNNENP2(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado incNp1decNp2(Estado e, int f) {
 		Estado ret = new Estado(e);
-		//TODO stub 
+		//TODO condición de aplicabilidad
+		ret.incrementarNNENP1(num, f);
+		ret.decrementarNNENP2(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
 	public static Estado decNp1incNp2(Estado e, int f) {
 		Estado ret = new Estado(e);
+		//TODO condición de aplicabilidad
 		ret.decrementarNNENP1(num, f);
 		ret.incrementarNNENP2(num, f);
+		//TODO recálculo cosas
 		return ret;
 	}
 	
--- bicing/src/bicing/SuccessorsHC.java
+++ bicing/src/bicing/SuccessorsHC.java
@@ -10,7 +10,18 @@ public class SuccessorsHC implements SuccessorFunction {
 	@Override
 	public List getSuccessors(Object arg0) {
 		// TODO Auto-generated method stub
-		return null;
+		Estado e = (Estado) arg0;
+		List<Estado> sucesores = new List<Estado>();
+		for (int i = 0; i < e.getvFurgonetas().length; ++i) {
+			//TODO falta mucho
+			sucesores.add(Operadores.decNeNp1(e, i));
+			sucesores.add(Operadores.incNeNp1(e, i));
+			sucesores.add(Operadores.decNeNp2(e, i));
+			sucesores.add(Operadores.incNeNp2(e, i));
+			sucesores.add(Operadores.incNp1decNp2(e, i));
+			sucesores.add(Operadores.decNp1incNp2(e, i));
+		}
+		return sucesores;
 	}
 
 }
