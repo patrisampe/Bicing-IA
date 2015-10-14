19cc2cf84d7c5b8cd9b83ae5f80da2e2f9af36f7
Octavi <octavi94@gmail.com>
2015-10-14 23:07:39 +0200

    Main millorat

--- bicing/src/bicing/Main.java
+++ bicing/src/bicing/Main.java
@@ -2,7 +2,37 @@ package bicing;
 
 import java.util.Scanner;
 
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
+
 public class Main {
+	
+	private static void HC() {
+		System.out.println("TSP HillClimbing\n");
+		Estado estado = new Estado();
+		estado = estado.estadoInicial(numF, nest);
+		SuccessorsHC sh = new SuccessorsHC();
+		EstadoFinal ef = new EstadoFinal();
+		FuncionHeuristica1 h1 = new FuncionHeuristica1();
+		Problem problem = new Problem(estado,sh, ef, h1);
+		Search search = new HillClimbingSearch();
+		Searchagent agent = new SearchAgent(problem, search);
+	}
+	
+	private static void SA() {
+		System.out.println("TSP Simulated Annealing\n");
+		Estado estado = new Estado();
+		estado = estado.estadoInicial(numF, nest);
+		SuccessorsSA sa= new SuccessorsSA();
+		EstadoFinal ef = new EstadoFinal();
+		FuncionHeuristica1 h1 = new FuncionHeuristica1();
+		Problem problem = new Problem(estado,sa, ef, h1);
+		Search search = new SimulatedAnnealingSearch(itMAX, it, k, lam);
+		Searchagent agent = new SearchAgent(problem, search);
+	}
 	
 	public static void main() {
 		Scanner s = new Scanner(System.in);
@@ -10,7 +40,8 @@ public class Main {
 		int nbic = s.nextInt();
 		int dem = s.nextInt();
 		int seed = s.nextInt();
-		GeneraProblema a=new GeneraProblema();
+		int numF = s.nextInt();
+		GeneraProblema a = new GeneraProblema();
 		a.CrearProblema(nest, nbic, dem, seed);
 		s.close();
 	}
