ad978e0ed34fe740a0b32ca613d9ef4273745406
Octavi <octavi94@gmail.com>
2015-10-17 12:37:04 +0200

    Heuristica1 modificada

--- bicing/src/bicing/FuncionHeuristica1.java
+++ bicing/src/bicing/FuncionHeuristica1.java
@@ -1,6 +1,8 @@
 package bicing;
 
 import aima.search.framework.HeuristicFunction;
+import IA.Bicing.Estacion;
+import IA.Bicing.Estaciones;
 
 /**
  * 
@@ -13,8 +15,22 @@ public class FuncionHeuristica1 implements HeuristicFunction {
 
 	public double getHeuristicValue(Object state) {
 		Estado actual = (Estado) state;
-		double dif = actual.getBicisBienColocadas() - actual.getBicisMalColocadas();
-		return (-dif);
+		
+		double no_usadas = 0.0;
+		double sum = 0.0;
+		
+		Estaciones estaciones = GeneraProblema.getEstaciones();
+		Struct[] vEstaciones = actual.getvEstaciones();
+		for (int i = 0; i < vEstaciones.length; ++i) {
+			Struct est = vEstaciones[i];
+			int delta = est.getBicisColocades() - est.getBicisAgafen();
+			Estacion Est = estaciones.get(i);
+			double aux = Est.getDemanda() - delta - Est.getNumBicicletasNext();
+			if (aux < 0.0) aux = 0.0;
+			sum += aux*aux;
+			no_usadas += Est.getNumBicicletasNoUsadas();
+		}
+		return (sum - no_usadas);
 	}
 	
 	public boolean equals(Object obj) {
