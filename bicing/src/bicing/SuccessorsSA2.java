a87c37053ea2cc4e069f95f269e8ccfc42b0290c
Yoel Cabo <yoel.cabo@est.fib.upc.edu>
2015-10-17 17:00:20 +0200

    SA adaptat a nums

---
+++ bicing/src/bicing/SuccessorsSA2.java
@@ -0,0 +1,15 @@
+package bicing;
+
+import java.util.List;
+
+import aima.search.framework.SuccessorFunction;
+
+public class SuccessorsSA2 implements SuccessorFunction {
+
+	@Override
+	public List getSuccessors(Object arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
