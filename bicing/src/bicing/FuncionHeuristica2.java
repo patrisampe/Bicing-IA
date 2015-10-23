8237aa3a10cd7c2dae6a8a118afec782d345e887
Octavi Pascual Sarda <octavi.pascual@c6s306pc63.fib.upc.es>
2015-10-23 13:11:55 +0200

    Heuristic OK?

--- bicing/src/bicing/FuncionHeuristica2.java
+++ bicing/src/bicing/FuncionHeuristica2.java
@@ -1,5 +1,6 @@
 package bicing;
 
+import IA.Bicing.Estacion;
 import aima.search.framework.HeuristicFunction;
 
 public class FuncionHeuristica2 implements HeuristicFunction {
@@ -13,8 +14,23 @@ public class FuncionHeuristica2 implements HeuristicFunction {
 	
 	public double getHeuristicValue(Object state) {
 		Estado actual = (Estado) state;
-		double dif = actual.getBicisBienColocadas() - actual.getBicisMalColocadas();
-		return (-dif);
+		double h1 = actual.getBicisBienColocadas() - actual.getBicisMalColocadas();
+		double h2 = 0.0, h3 = 0.0;
+		for (Furgoneta f : actual.getvFurgonetas()) {
+			Estacion e = f.getEstacioE();
+			double d1 = Math.min(e.getNumBicicletasNoUsadas(),Math.max(0,e.getNumBicicletasNext()-e.getDemanda()));
+			Estacion e1 = f.getEstacioP1();
+			Estacion e2 = f.getEstacioP2();
+			int m1 = e1.getDemanda() - e1.getNumBicicletasNext();
+			if (m1 < 0) m1 = 0;
+			int m2 = e2.getDemanda() - e2.getNumBicicletasNext();
+			if (m2 < 0) m2 = 0;
+			double d2 = m1+m2;
+			h2 += (Math.min(d1,d2) - (f.getNp1() + f.getNp2()))/(double)10;
+			h3 += (f.getNp1() + f.getNp2())/(double)5;
+		
+		}
+		return (-(h1+h2+h3));
 	}
 	
 	public boolean equals(Object obj) {
